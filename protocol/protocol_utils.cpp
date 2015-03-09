240aefc9ade4688b1be20d992e5dd1064a0380cb
Marcin Kolny <marcin.kolny@gmail.com>
2015-03-09 20:30:10 +0100

    add pack method

--- protocol/protocol_utils.cpp
+++ protocol/protocol_utils.cpp
@@ -9,11 +9,18 @@
 
 namespace SnowCookie {
 
-bool check_crc (unsigned char *data, int size, char expected_crc)
+static char calculate_crc (unsigned char * data, int size)
 {
 	char crc = 0;
 	for (int i = 0; i < size; i++)
 		crc ^= data [i];
+
+	return crc;
+}
+
+bool check_crc (unsigned char *data, int size, char expected_crc)
+{
+	char crc = calculate_crc (data, size);
 
 	return crc == expected_crc;
 }
@@ -32,6 +39,36 @@ int unpack_frame (unsigned char *data, int size, char substitute_char, char xor_
 		else
 			data [new_size++] = data [i];
 	}
+
+	return new_size;
+}
+
+int pack_frame (unsigned char *data, int size, unsigned char* destination, char substitute_char, char xor_char, char end_char)
+{
+	int new_size = 0;
+	char crc = calculate_crc (data, size);
+
+	for (int i = 0; i < size; i++)
+	{
+		if (data[i] != end_char && data[i] != substitute_char)
+			destination[new_size++] = data[i];
+		else
+		{
+			destination [new_size++] = substitute_char;
+			destination [new_size++] = data [i] ^ xor_char;
+		}
+	}
+
+	// todo refactor me!
+	if (crc != end_char && crc != substitute_char)
+		destination[new_size++] = crc;
+	else
+	{
+		destination [new_size++] = substitute_char;
+		destination [new_size++] = crc ^ xor_char;
+	}
+
+	destination[new_size++] = end_char;
 
 	return new_size;
 }
--- protocol/protocol_utils.h
+++ protocol/protocol_utils.h
@@ -12,7 +12,10 @@ namespace SnowCookie {
 
 bool check_crc (unsigned char *data, int size, char expected_crc);
 
-int unpack_frame (unsigned char *data, int size, char substitute_char, char xor_char);
+int unpack_frame (unsigned char *frame, int size, char substitute_char, char xor_char);
+
+// this method adds crc and end frame character, returns new data size
+int pack_frame (unsigned char *data, int size, unsigned char* destination, char substitute_char, char xor_char, char end_char);
 
 }
 
