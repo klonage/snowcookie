1cedf0d0e00d3f19cbe552928ece3a44bed6631e
Marcin Kolny <marcin.kolny@gmail.com>
2015-03-14 00:48:05 +0100

    create log in /home/root/edison_logs directory

--- edison-board/dataglutton/log_manager.cpp
+++ edison-board/dataglutton/log_manager.cpp
@@ -6,9 +6,12 @@
  */
 
 #include "log_manager.h"
+#include "logger.h"
 
 #include <unistd.h>
 #include <sys/stat.h>
+
+#include <cstring>
 
 #include <stdexcept>
 #include <chrono>
@@ -31,6 +34,7 @@ void LogManager::start_log (const std::string& filename)
 	if (!file.is_open())
 		throw std::runtime_error("cannot open file: " + filename);
 
+	Logger::log ("start loggin in file ", filename);
 	log_files.push_back (filename);
 
 	paused = false;
@@ -108,11 +112,64 @@ std::string LogManager::generate_unique_filename(std::string name)
 	return new_name;
 }
 
+static int do_mkdir(const char *path, mode_t mode)
+{
+	struct stat st;
+	int status = 0;
+
+	if (stat(path, &st) != 0)
+	{
+		if (mkdir (path, mode) != 0 && errno != EEXIST)
+			status = -1;
+	}
+	else if (!S_ISDIR (st.st_mode))
+	{
+		errno = ENOTDIR;
+		status = -1;
+	}
+
+	return status;
+}
+
+static int mkpath(const char *path, mode_t mode)
+{
+	char *pp;
+	char *sp;
+	int status;
+	char *copypath = strdup(path);
+
+	status = 0;
+	pp = copypath;
+	while (status == 0 && (sp = strchr(pp, '/')) != 0)
+	{
+		if (sp != pp)
+		{
+			*sp = '\0';
+			status = do_mkdir(copypath, mode);
+			*sp = '/';
+		}
+		pp = sp + 1;
+	}
+	if (status == 0)
+		status = do_mkdir(path, mode);
+	free (copypath);
+	return status;
+}
+
 std::string LogManager::get_dated_filename ()
 {
+	// todo quick hack, Intel EDISON & Linux Yocto only!
+	char mbstr[64];
+	int ctr = 0;
+	std::string dir = "/home/root/edison_logs/";
+	int status = mkpath (dir.c_str(), 0777);
+	if (status == 0)
+	{
+		ctr = snprintf (mbstr, 64, "%s", dir.c_str());
+	}
+
 	std::time_t t = std::time(NULL);
-	char mbstr[30];
-	if (std::strftime(mbstr, sizeof(mbstr), "log_%Y-%m-%d-%H-%M-%S.txt", std::localtime(&t))) {
+	if (std::strftime(mbstr + ctr, sizeof(mbstr) - ctr, "log_%Y-%m-%d-%H-%M-%S.txt", std::localtime(&t))) {
 		return std::string(mbstr);
 	}
 	return "log_unknown";
